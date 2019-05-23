882c5467d8e364a7309a90bad4a0066cbb237050
Bjarni Einarrson <Bjarni bjarnie3@googlemail.com>
2019-05-23 13:22:06 +0000

    BensínVerkefni
    BensínVerkefni

--- js/index.js
+++ js/index.js
@@ -2,17 +2,10 @@
 let init = () => {
   getData();
 }
-
-// Notkun: getData(lang, arrivals)
-// Fyrir: ekkert
-// eftir: Búið að hreinsa töflu á forsíðunni
-//        og skrifa út ný gögn sem eru sótt í gegnum API
-// lang="en" eða lang="is" og type="arrivals" eða type="departments"
-
 let getData = (type="bensin") => {
   console.log("success");
   $('#petrol-table').html(' ');
-  $.get(`http://apis.is/petrol`, (data) => {
+  $.get(`https://apis.is/petrol`, (data) => {
     data.results.map((results) => {
       console.log("success 1");
       if(type == "bensin"){
@@ -27,23 +20,9 @@ let getData = (type="bensin") => {
           <td>${results.name}</td>
           <td>${results.diesel}</td></tr>`)
       }
-      /*<td>${results.lat}</td>
-        <td>${results.lon}</td>
-        <td>${results.key}</td> */
     });
   });
 }
-/*{
-"company":"Atlantsolía",
-"name":"Baldursnes Akureyri"
-"bensin95":191.3,
-"diesel":184.3,
-"bensin95_discount":188.3,
-"diesel_discount":181.3,
-"geo":{"lat":65.69913,"lon":-18.135231},
-"key":"ao_000",
-}
-*/
 let btn_is = document.getElementById("btn_is");
 let btn_gas = document.getElementById("btn_gas");
 
@@ -56,9 +35,6 @@ btn_gas.onclick = function () {
     $("#company").text("Fyrirtæki");
     $("#name").text("Staðsetning");
     $("#Bensin95").text("Dísel");
-    /*$("#lat").text("Áætlaður tími");
-    $("#lon").text("Staðfestur tími");
-    $("#key").text("Staða");*/
     $("#btn_gas").text("Bensín");
   }
   else {
@@ -67,32 +43,7 @@ btn_gas.onclick = function () {
     $("#company").text("Fyrirtæki");
     $("#name").text("Staðsetning");
     $("#Bensin95").text("Bensín");
-    /*$("#lat").text("Áætlaður tími");
-    $("#lon").text("Staðfestur tími");
-    $("#key").text("Staða");*/
     $("#btn_gas").text("Dísel");
   }
 }
-
-/*btn_dep.onclick = function () {
-  getData(localStorage.getItem("lang"), "departures");
-  if(localStorage.getItem("lang") === "is"){
-    $("#name").text("Til");
-  }
-  else{
-    $("#name").text("To");
-  }
-
-}*/
-
-/*btn_arr.onclick = function () {
-  getData(localStorage.getItem("lang"), "arrivals");
-  if(localStorage.getItem("lang") === "is"){
-    $("#to_From").text("Frá");
-  }
-  else{
-    $("#to_From").text("From");
-  }
-}
-*/
 init();
